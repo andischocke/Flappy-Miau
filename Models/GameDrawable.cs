ed906cec49d4a0944af72b11cfab55f39547f1d9
Andreas <Andreas.Henneberger@outlook.com>
2024-06-29 22:39:31 +0200

    Add moving obstacles

--- Models/GameDrawable.cs
+++ Models/GameDrawable.cs
@@ -7,6 +7,7 @@ public class GameDrawable : IDrawable
     private double DisplayWidth { get; set; }
     private double DisplayHeight { get; set; }
     private (RectF Upper, Rect Lower) Bounds { get; set; }
+    private List<RectF> Obstacles { get; set; }
     public int Score { get; set; }
 
     public GameDrawable()
@@ -15,8 +16,9 @@ public class GameDrawable : IDrawable
         DisplayWidth = DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density;
         DisplayHeight = (DeviceDisplay.MainDisplayInfo.Height - 100) / DeviceDisplay.MainDisplayInfo.Density;
 
-        // Set the bounds
+        // Set terrain elements
         Bounds = (new RectF(0, 0, (float)DisplayWidth, 20), new RectF(0, (float)DisplayHeight - 20, (float)DisplayWidth, 20));
+        Obstacles = new List<RectF>();
     }
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
@@ -25,6 +27,43 @@ public class GameDrawable : IDrawable
         canvas.FillColor = Colors.Green;
         canvas.FillRectangle(Bounds.Upper);
         canvas.FillRectangle(Bounds.Lower);
+
+        // Spawn new obstacles when last Obstacle is in the middle of the screen
+        if (Obstacles.Count == 0 || Obstacles.Last().X < DisplayWidth * 0.5)
+        {
+            // Set obstacle size
+            float obstacleWidth = 100f;
+            float obstacleHeight = (float)DisplayHeight * (0.2f + (float)new Random().NextDouble() * 0.6f);
+
+            // Set obstacle position
+            float obstacleX = (float)DisplayWidth;
+            float obstacleY = (float)DisplayHeight - obstacleHeight;
+
+            // Add top and bottom obstacles
+            Obstacles.Add(new RectF(obstacleX, 0, obstacleWidth, obstacleY - 200));
+            Obstacles.Add(new RectF(obstacleX, obstacleY, obstacleWidth, obstacleHeight));
+        }
+
+        // Update the obstacles
+        for (int i = Obstacles.Count - 1; i >= 0; i--)
+        {
+            // Create a copy of the current obstacle because RectF is a struct
+            RectF obstacle = Obstacles[i];
+            // Move the obstacle to the left
+            obstacle.X -= 5f;
+            // Checks if the obstacle has completely moved off the screen to the left
+            if (obstacle.X + obstacle.Width < 0)
+            {
+                Obstacles.RemoveAt(i);
+            }
+            else
+            {
+                // Assign the updated obstacle back to the list
+                Obstacles[i] = obstacle;
+                // Draw the obstacle
+                canvas.FillRectangle(obstacle);
+            }
+        }
 
         // Draw the score
         canvas.FontColor = Colors.White;
